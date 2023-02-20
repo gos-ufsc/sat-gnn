2e8de75a68a94a810b72d7c38c5d52225b37ce6b
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-02-20 11:52:33 -0300

    consolidate functions for getting BSP cut and recover feasibility for BMP

--- src/problem.py
+++ src/problem.py
@@ -284,3 +284,140 @@ def get_soc(X, instance, r=None):
         soc[:,t] = soc[:,t-1] + (ef / q) * (i[:,t] / 60)
 
     return soc
+
+def get_benders_cut(instance, solucao, verbose=False):
+    subproblem = gurobipy.Model()
+    if verbose:
+        subproblem.Params.LogToConsole = 1
+
+    lmbd0 = {}
+    lmbd1 = {}
+    lmbd2 = {}
+    lmbd3 = {}
+    lmbd4 = {}
+    lmbd5 = {}
+    lmbd6 = {}
+    lmbd7 = {}
+
+    J = instance['jobs'][0]
+    T = instance['tamanho'][0]
+    uso_p = instance['uso_p']
+    recurso_p = instance['recurso_p']
+
+    soc_inicial = 0.7
+    limite_inferior = 0.0
+    ef = 0.9
+    v_bat = 3.6
+    q = 5
+    bat_usage = 5
+
+    for t in range(T):
+        lmbd1[t] = subproblem.addVar(name="lmbd1(%s)" % t, lb=0, vtype=GRB.CONTINUOUS) # alpha 1 
+        lmbd2[t] = subproblem.addVar(name="lmbd2(%s)" % t, lb=-GRB.INFINITY, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS) # b 2 
+        lmbd3[t] = subproblem.addVar(name="lmbd3(%s)" % t, lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS) # b 3
+        lmbd4[t] = subproblem.addVar(name="lmbd4(%s)" % t, lb=-GRB.INFINITY, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS) # soc - i 4
+        lmbd5[t] = subproblem.addVar(name="lmbd5(%s)" % t, lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS) # soc - i 5
+        lmbd6[t] = subproblem.addVar(name="lmbd6(%s)" % t, lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS) # limite inferior 6
+        lmbd7[t] = subproblem.addVar(name="lmbd7(%s)" % t, lb=0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS) # limite inferior 6
+
+    for t in range(T):
+        subproblem.addConstr((bat_usage * v_bat)*lmbd1[t] + lmbd7[t] >= 0)
+        subproblem.addConstr(lmbd2[t] - lmbd3[t]/v_bat == 0)
+        subproblem.addConstr(lmbd3[t] -  (ef / q)*(1/60) * lmbd4[t] == 0)
+
+    for t in range(T-1):
+            subproblem.addConstr(lmbd4[t] - lmbd4[t+1] + (-lmbd5[t] + lmbd6[t]) == 0)
+
+    subproblem.addConstr(lmbd4[T-1] + (-lmbd5[T-1] + lmbd6[T-1]) == 0)
+    subproblem.setParam('InfUnbdInfo', 1)
+    subproblem.Params.LogToConsole = 0
+    subproblem.update()
+
+    obj = 0
+
+    for t in range(T):
+        lhs = float(sum(uso_p[j] * solucao[j][t] for j in range(J)))
+        obj += lmbd1[t] * ((recurso_p[t] + bat_usage * v_bat) - lhs) + lmbd2[t] * (recurso_p[t] - lhs) - 0.0*lmbd5[t] + lmbd6[t] + lmbd7[t]
+    obj += lmbd4[0] * 0.7
+    subproblem.setObjective(obj, GRB.MINIMIZE)
+
+    subproblem.update()
+    subproblem.optimize()
+
+    if subproblem.status == 5:
+        teste = gurobipy.Model()
+        A = teste.addVars(J,T, vtype=GRB.BINARY, name="A")
+        teste.update()
+        cut = 0
+        for t in range(T):
+            lhs = sum(uso_p[j] * A[j,t] for j in range(J))
+            cut += (lmbd1[t].getAttr(GRB.Attr.UnbdRay) * ((recurso_p[t] + bat_usage * v_bat) - lhs) + lmbd2[t].getAttr(GRB.Attr.UnbdRay) * (recurso_p[t] - lhs)
+                    - lmbd5[t].getAttr(GRB.Attr.UnbdRay)*0.0 + lmbd6[t].getAttr(GRB.Attr.UnbdRay) + lmbd7[t].getAttr(GRB.Attr.UnbdRay) )
+        cut += lmbd4[0].getAttr(GRB.Attr.UnbdRay) * 0.7
+        cut = str(cut)
+
+        indices = {}
+        corte = []
+        # Extracting the indices and coefficients using regular expression
+        for match in re.finditer(r"([+-]?\d+\.\d+) A\[(\d+),(\d+)\]", cut):
+            coefficient = float(match.group(1))
+            i = int(match.group(2))
+            j = int(match.group(3))
+            indices[(i,j)] = float(coefficient)
+            #if float(coefficient) != 0:
+            #    print(i,j,coefficient)
+            #    #print(pato)
+
+        # assuming the cut is of the form w^T x >= b
+        cut_w = np.zeros_like(solucao)
+        for (i, j), w_ij in indices.items():
+            cut_w[i,j] = w_ij
+        cut_b = float(cut.split(' ')[0])
+        # indices['const'] = cut.split(' ')[0]
+        # #corte = [val for val in indices.values()]
+        # for j in range(J):
+        #     for t in range(T):
+        #         corte.append(indices[(j,t)])
+        # corte.append(indices['const'])
+
+        return cut_w, cut_b
+    elif subproblem.status == 2:
+        return None
+    else:
+        print('ERROR: status ', subproblem.status)
+
+def get_feasible(model, incumbent):
+    model_ = model.copy()
+
+    expr = 0
+    for j in range(J):
+        for t in range(T):
+            #if model.getVarByName("x(%s,%s,%s)" % (s,j,t)).x > 0.5:
+            if incumbent[j][t] > 0.5:
+                expr += (1 - model_.getVarByName("x(%s,%s)" % (j,t)))
+            else:
+                expr += model_.getVarByName("x(%s,%s)" % (j,t))
+            #if incumbent_phi[j][t] > 0.5:
+            #    expr += (1 - model.getVarByName("phi(%s,%s)" % (j,t)))
+            #else:
+            #    expr += model.getVarByName("phi[%s,%s,%s]" % (j,t))
+
+    theta = model_.addVar(vtype=GRB.CONTINUOUS, name="theta")
+    model_.addConstr(expr <= theta)
+    M1 = 1000
+    model_.setObjective(M1*theta, GRB.MINIMIZE)
+    model_.update()
+    model_.optimize()
+
+    if model_.status == 2:
+        feas_incumbent = np.zeros_like(incumbent)
+        for var in model_.getVars():
+            try:
+                j, t = re.fullmatch(r"x\((\d+),(\d+)\)", var.getAttr(GRB.Attr.VarName)).groups()
+            except AttributeError:
+                continue
+            j = int(j)
+            t = int(t)
+            feas_incumbent[j,t] = var.X
+
+    return feas_incumbent
