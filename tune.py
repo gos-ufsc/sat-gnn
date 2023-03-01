f8f12750d6f4eb7cc3d466f168ce2ec1ee2bba3c
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-03-01 14:27:43 -0300

    backup study after every trial

--- tune.py
+++ tune.py
@@ -21,23 +21,23 @@ if __name__ == '__main__':
         wandb_project = None
     else:
         wandb_project = 'sat-gnn'
-    wandb_group = 'EarlyFixingInstance-tuning'
+    wandb_group = 'EarlyFixingInstance-tuning3'
 
     # instances_fpaths = list(Path('data/raw/').glob('97_9*.jl'))
-    instances_fpaths = [fp for fp in Path('data/raw/').glob('97_9*.jl') if (fp.name != '97_9_2.jl') and (fp.name != '97_9_10.jl')]
+    instances_fpaths = [fp for fp in Path('data/raw/').glob('97_9*.jl')]
     with open('97_9_opts.pkl', 'rb') as f:
         opts = pickle.load(f)
 
-    study_fpath = Path('study2.pkl')
+    study_fpath = Path('study3.pkl')
+
+    if study_fpath.exists():
+        with open(study_fpath, 'rb') as f:
+            study = pickle.load(f)
+    else:
+        study = optuna.create_study(study_name='early fixing')
 
     for _ in range(100):
-        if study_fpath.exists():
-            with open(study_fpath, 'rb') as f:
-                study = pickle.load(f)
-        else:
-            study = optuna.create_study(study_name='early fixing')
+        study.optimize(get_objective(instances_fpaths, opts, wandb_project=wandb_project), n_trials=1)
 
-        study.optimize(get_objective(instances_fpaths, opts, wandb_project=wandb_project), n_trials=2)
-
-        with open('study.pkl', 'wb') as f:
+        with open(study_fpath, 'wb') as f:
             pickle.dump(study, f)
