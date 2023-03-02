4ce664e635e320f8dbdc90ccc3885cb02caa5b71
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-03-02 09:33:35 -0300

    fix experiment 1 with current implementation

--- experiment1.py
+++ experiment1.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+
+import numpy as np
 import torch
 
-from src.net import GCN
+from src.net import JobGCN
 from src.trainer import FactibilityClassificationTrainer
 from src.utils import debugger_is_active
-
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -17,12 +18,14 @@ if __name__ == '__main__':
 
     # Classification of the graph+candidate solution into feasible or not
     FactibilityClassificationTrainer(
-        GCN(2, 1),
-        # instance_fpath=Path('data/raw/97_9.jl'),
+        JobGCN(2, 1),
+        instance_fpath=Path('data/raw/97_9.jl'),
         batch_size=2**5,
-        epochs=1000,
+        epochs=100,
         wandb_project=wandb_project,
         wandb_group='Experiment1 - Job',
+        # lr_scheduler='MultiplicativeLR',
+        # lr_scheduler_params={'lr_lambda': lambda e: 1 - np.exp(10*(e/E - 1))},
         random_seed=None,
         device=device,
     ).run()
