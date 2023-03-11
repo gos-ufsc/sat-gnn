02191c13d72822caba25dd5daf644bbc0b85f9b1
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-03-10 21:19:39 -0300

    optuna on validation split

--- tune.py
+++ tune.py
@@ -21,14 +21,15 @@ if __name__ == '__main__':
         wandb_project = None
     else:
         wandb_project = 'sat-gnn'
-    wandb_group = 'EarlyFixingInstance-tuning3'
+    wandb_group = 'EarlyFixingInstance-validation'
 
     # instances_fpaths = list(Path('data/raw/').glob('97_9*.jl'))
-    instances_fpaths = [fp for fp in Path('data/raw/').glob('97_9*.jl')]
+    instances_fpaths = [fp for fp in Path('data/raw/').glob('97_9*.jl') if fp.name not in ['97_9_9.jl', '97_9_6.jl']]
+    print(instances_fpaths)
     with open('97_9_opts.pkl', 'rb') as f:
         opts = pickle.load(f)
 
-    study_fpath = Path('study3.pkl')
+    study_fpath = Path('study_val.pkl')
 
     if study_fpath.exists():
         with open(study_fpath, 'rb') as f:
@@ -37,7 +38,7 @@ if __name__ == '__main__':
         study = optuna.create_study(study_name='early fixing')
 
     for _ in range(100):
-        study.optimize(get_objective(instances_fpaths, opts, wandb_project=wandb_project), n_trials=1)
+        study.optimize(get_objective(instances_fpaths, opts, wandb_project=wandb_project, wandb_group=wandb_group), n_trials=1)
 
         with open(study_fpath, 'wb') as f:
             pickle.dump(study, f)
