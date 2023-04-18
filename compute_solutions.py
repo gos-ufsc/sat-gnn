06d21e735d30971fb22c1c79ea7512d7ce8502c2
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-04-18 15:39:17 -0300

    compute solutions and store as .npz

--- compute_solutions.py
+++ compute_solutions.py
@@ -9,15 +9,15 @@ from src.problem import get_model
 
 
 if __name__ == '__main__':
-    instances_fps = list(Path('data/raw').glob('12*.json'))
+    instances_fps = list(Path('data/raw').glob('97_*.json'))
 
-    dst_fpath = Path('data/interim')/'solutions.pkl'
+    dst_dir = Path('data/interim')
 
     try:
-        with open(dst_fpath, 'rb') as f:
-            solutions = pickle.load(f)
+        solutions_fpaths = dst_dir.glob('*_sols.npz')
+        solutions = [fp.name[:-len('_sols.npz')] for fp in solutions_fpaths]
 
-        instances_fps = [f for f in instances_fps if f[:-len('.json')] not in solutions.keys()]
+        instances_fps = [f for f in instances_fps if f.name[:-len('.json')] not in solutions]
     except FileNotFoundError:
         solutions = dict()
 
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
         jobs = list(range(instance['jobs']))
 
-        model = get_model(jobs, instance, coupling=True, new_ineq=False, timeout=60)
+        model = get_model(instance, coupling=True, new_ineq=False, timeout=60)
         model.setParam('PoolSearchMode', 2)
         model.setParam('PoolSolutions', 500)
         model.update()
@@ -42,14 +42,12 @@ if __name__ == '__main__':
             model.Params.SolutionNumber = i
             objs.append(model.PoolObjVal)
             # sol = np.array([v.xn for v in model.getVars() if 'x(' in v.VarName])
-            sol = np.array([v.xn for v in model.getVars()])
+            sol = np.array([v.xn for v in model.getVars() if ('x(' in v.VarName) or ('phi(' in v.VarName)])
+            # sol = np.array([v.xn for v in model.getVars()])
             sols.append(sol)
         model.Params.SolutionNumber = 0
 
-        solutions[instance_name] = {
-            'sols': np.array(sols),
-            'objs': np.array(objs),
-        }
+        sols = np.array(sols)
+        objs = np.array(objs)
 
-        with open(dst_fpath, 'wb') as f:
-            pickle.dump(solutions, f)
+        np.savez(dst_dir/(instance_name + '_sols.npz'), sols, objs)
