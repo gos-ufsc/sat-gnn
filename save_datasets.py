5083a1181c3c30a1dd1c34e0acda3d16d664d402
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-08-16 11:05:07 -0300

    add our datasets with 100+ instances

--- save_datasets.py
+++ save_datasets.py
@@ -4,28 +4,43 @@ from src.dataset import MultiTargetDataset, OptimalsDataset, OptimalsWithZetaDat
 
 
 if __name__ == '__main__':
-    instances_fpaths = list(Path('data/raw/').glob('97_*.json'))
+    # instances_fpaths = list(Path('data/raw/').glob('97_*.json'))
 
-    ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
-                            sols_dir='/home/bruno/sat-gnn/data/interim',
-                            split='all')
-    ds.maybe_initialize()
-    ds.save_dataset('data/processed/multitarget_97_all.hdf5')
+    # ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
+    #                         sols_dir='/home/bruno/sat-gnn/data/interim',
+    #                         split='all')
+    # ds.maybe_initialize()
+    # ds.save_dataset('data/processed/multitarget_97_all.hdf5')
 
-    ds = VarOptimalityDataset(instances_fpaths=instances_fpaths,
-                              sols_dir='/home/bruno/sat-gnn/data/interim',
-                              split='all')
-    ds.maybe_initialize()
-    ds.save_dataset('data/processed/varoptimality_97_all.hdf5')
+    # ds = VarOptimalityDataset(instances_fpaths=instances_fpaths,
+    #                           sols_dir='/home/bruno/sat-gnn/data/interim',
+    #                           split='all')
+    # ds.maybe_initialize()
+    # ds.save_dataset('data/processed/varoptimality_97_all.hdf5')
 
-    ds = OptimalsDataset(instances_fpaths=instances_fpaths,
-                         sols_dir='/home/bruno/sat-gnn/data/interim',
-                         split='all')
-    ds.maybe_initialize()
-    ds.save_dataset('data/processed/optimals_97_all.hdf5')
+    # ds = OptimalsDataset(instances_fpaths=instances_fpaths,
+    #                      sols_dir='/home/bruno/sat-gnn/data/interim',
+    #                      split='all')
+    # ds.maybe_initialize()
+    # ds.save_dataset('data/processed/optimals_97_all.hdf5')
 
-    ds = OptimalsWithZetaDataset(instances_fpaths=instances_fpaths,
-                                 sols_dir='/home/bruno/sat-gnn/data/interim',
-                                 split='all')
+    # ds = OptimalsWithZetaDataset(instances_fpaths=instances_fpaths,
+    #                              sols_dir='/home/bruno/sat-gnn/data/interim',
+    #                              split='all')
+    # ds.maybe_initialize()
+    # ds.save_dataset('data/processed/optimalszeta_97_all.hdf5')
+
+    ds = MultiTargetDataset(
+        [fp for fp in Path('data/raw/').glob('125_*.json') if (int(fp.name.split('_')[1]) < 20)]
+        + [fp for fp in Path('data/raw/').glob('97_*.json') if (int(fp.name.split('_')[1]) < 20)],
+        split='all',
+    )
     ds.maybe_initialize()
-    ds.save_dataset('data/processed/optimalszeta_97_all.hdf5')
+    ds.save_dataset('data/processed/multitarget_125_97_small_all_200.hdf5')
+
+    # ds = MultiTargetDataset(
+    #     [fp for fp in Path('data/raw/').glob('125_*.json') if int(fp.name.split('_')[1]) >= 20],
+    #     split='all',
+    # )
+    # ds.maybe_initialize()
+    # ds.save_dataset('data/processed/multitarget_125_large_all.hdf5')
