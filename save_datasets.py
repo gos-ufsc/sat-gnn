fe609d925b3443d794186ca35cd004c841ad9af6
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-08-29 12:27:37 -0300

    save training and validation sets for multitarget

--- save_datasets.py
+++ save_datasets.py
@@ -4,60 +4,25 @@ from src.dataset import MultiTargetDataset, OptimalsDataset, OptimalsWithZetaDat
 
 
 if __name__ == '__main__':
-    # instances_fpaths = list(Path('data/raw/').glob('97_*.json'))
+    instances_fpaths = [fp for fp in Path('data/raw/').glob('125_*.json')
+                        if (int(fp.name.split('_')[1]) < 20) and
+                           (int(fp.name.split('_')[2].replace('.json', '')) < 200)]
 
-    # ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
-    #                         sols_dir='/home/bruno/sat-gnn/data/interim',
-    #                         split='all')
-    # ds.maybe_initialize()
-    # ds.save_dataset('data/processed/multitarget_97_all.hdf5')
-
-    # ds = VarOptimalityDataset(instances_fpaths=instances_fpaths,
-    #                           sols_dir='/home/bruno/sat-gnn/data/interim',
-    #                           split='all')
-    # ds.maybe_initialize()
-    # ds.save_dataset('data/processed/varoptimality_97_all.hdf5')
+    ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
+                            sols_dir='/home/bruno/sat-gnn/data/interim')
+    ds.maybe_initialize()
+    ds.save_dataset('data/processed/multitarget_125_train.hdf5')
 
     # ds = OptimalsDataset(instances_fpaths=instances_fpaths,
-    #                      sols_dir='/home/bruno/sat-gnn/data/interim',
-    #                      split='all')
+    #                      sols_dir='/home/bruno/sat-gnn/data/interim')
     # ds.maybe_initialize()
-    # ds.save_dataset('data/processed/optimals_97_all.hdf5')
+    # ds.save_dataset('data/processed/optimals_125_small_train.hdf5')
 
-    # ds = OptimalsWithZetaDataset(instances_fpaths=instances_fpaths,
-    #                              sols_dir='/home/bruno/sat-gnn/data/interim',
-    #                              split='all')
-    # ds.maybe_initialize()
-    # ds.save_dataset('data/processed/optimalszeta_97_all.hdf5')
+    instances_fpaths = [fp for fp in Path('data/raw/').glob('125_*.json')
+                        if (int(fp.name.split('_')[1]) >= 20) and
+                           (int(fp.name.split('_')[2].replace('.json', '')) < 20)]
 
-    # ds = MultiTargetDataset(
-    #     [fp for fp in Path('data/raw/').glob('125_*.json') if (int(fp.name.split('_')[1]) < 20)]
-    #     + [fp for fp in Path('data/raw/').glob('97_*.json') if (int(fp.name.split('_')[1]) < 20)],
-    # )
-    # ds.maybe_initialize()
-    # ds.save_dataset('data/processed/multitarget_125_97_small_all_200.hdf5')
-
-    ds = SolutionFeasibilityDataset(
-        [fp for fp in Path('data/raw/').glob('125_*.json') if (int(fp.name.split('_')[1]) < 20) and (int(fp.name.split('_')[2].replace('.json', '')) < 200)],
-    )
+    ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
+                            sols_dir='/home/bruno/sat-gnn/data/interim')
     ds.maybe_initialize()
-    ds.save_dataset('data/processed/feasibility_125_train.hdf5')
-
-    ds = SolutionFeasibilityDataset(
-        [fp for fp in Path('data/raw/').glob('125_*.json') if (int(fp.name.split('_')[1]) >= 20) and (int(fp.name.split('_')[2].replace('.json', '')) < 20)],
-    )
-    ds.maybe_initialize()
-    ds.save_dataset('data/processed/feasibility_125_val.hdf5')
-
-    ds = SolutionFeasibilityDataset(
-        [fp for fp in Path('data/raw/').glob('125_*.json') if (int(fp.name.split('_')[1]) >= 20) and (int(fp.name.split('_')[2].replace('.json', '')) < 40) and (int(fp.name.split('_')[2].replace('.json', '')) >= 20)],
-    )
-    ds.maybe_initialize()
-    ds.save_dataset('data/processed/feasibility_125_test.hdf5')
-
-    # ds = MultiTargetDataset(
-    #     [fp for fp in Path('data/raw/').glob('125_*.json') if int(fp.name.split('_')[1]) >= 20],
-    #     split='all',
-    # )
-    # ds.maybe_initialize()
-    # ds.save_dataset('data/processed/multitarget_125_large_all.hdf5')
+    ds.save_dataset('data/processed/multitarget_125_val.hdf5')
