0c7598594bd0d8095308796cdf080b116f74ea82
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-08-30 20:13:07 -0300

    save OptimalsDataset

--- save_datasets.py
+++ save_datasets.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from src.dataset import MultiTargetDataset, OptimalsDataset, OptimalsWithZetaDataset, VarOptimalityDataset, SolutionFeasibilityDataset
+from src.dataset import MultiTargetDataset, OptimalsDataset
 
 
 if __name__ == '__main__':
@@ -8,21 +8,26 @@ if __name__ == '__main__':
                         if (int(fp.name.split('_')[1]) < 20) and
                            (int(fp.name.split('_')[2].replace('.json', '')) < 200)]
 
-    ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
-                            sols_dir='/home/bruno/sat-gnn/data/interim')
-    ds.maybe_initialize()
-    ds.save_dataset('data/processed/multitarget_125_train.hdf5')
+   #  ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
+   #                          sols_dir='/home/bruno/sat-gnn/data/interim')
+   #  ds.maybe_initialize()
+   #  ds.save_dataset('data/processed/multitarget_125_train.hdf5')
 
-    # ds = OptimalsDataset(instances_fpaths=instances_fpaths,
-    #                      sols_dir='/home/bruno/sat-gnn/data/interim')
-    # ds.maybe_initialize()
-    # ds.save_dataset('data/processed/optimals_125_small_train.hdf5')
+    ds = OptimalsDataset(instances_fpaths=instances_fpaths,
+                         sols_dir='/home/bruno/sat-gnn/data/interim')
+    ds.maybe_initialize()
+    ds.save_dataset('data/processed/optimals_125_train.hdf5')
 
     instances_fpaths = [fp for fp in Path('data/raw/').glob('125_*.json')
                         if (int(fp.name.split('_')[1]) >= 20) and
                            (int(fp.name.split('_')[2].replace('.json', '')) < 20)]
 
-    ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
-                            sols_dir='/home/bruno/sat-gnn/data/interim')
+   #  ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
+   #                          sols_dir='/home/bruno/sat-gnn/data/interim')
+   #  ds.maybe_initialize()
+   #  ds.save_dataset('data/processed/multitarget_125_val.hdf5')
+
+    ds = OptimalsDataset(instances_fpaths=instances_fpaths,
+                         sols_dir='/home/bruno/sat-gnn/data/interim')
     ds.maybe_initialize()
-    ds.save_dataset('data/processed/multitarget_125_val.hdf5')
+    ds.save_dataset('data/processed/optimals_125_val.hdf5')
