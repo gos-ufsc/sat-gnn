c632fc14523208454ccefb83ab93044fa10b7a00
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-08-30 20:16:37 -0300

    train SatGNN with single target (quasi-optimal)

--- train.py
+++ train.py
@@ -31,74 +31,72 @@ if __name__ == '__main__':
 
     # Early fixing the solution to all jobs, including coupling constraints
     for _ in range(1):
-        # net = InstanceGCN(
-        #     readout_op=None,
+        # if debugger_is_active():
+        #     train_dataset = MultiTargetDataset(
+        #         [fp for fp in Path('data/raw/').glob('125_*.json')
+        #          if (int(fp.name.split('_')[1]) < 20) and
+        #             (int(fp.name.split('_')[2].replace('.json', '')) < 10)]
+        #     )
+        #     val_dataset = train_dataset
+        # else:
+        #     train_dataset = MultiTargetDataset.from_file_lazy('data/processed/multitarget_125_train.hdf5')
+        #     val_dataset = MultiTargetDataset.from_file_lazy('data/processed/multitarget_125_val.hdf5')
+
+        # net = OptSatGNN(
+        #     n_h_feats=256,
+        #     single_conv_for_both_passes=True,
+        #     n_passes=3,
+        #     conv1='SAGEConv',
+        #     conv1_kwargs={ 'aggregator_type': 'pool' },
+        #     conv2=None,
+        #     conv2_kwargs=dict(),
         # )
 
+        # MultiTargetTrainer(
+        #     net.double(),
+        #     training_dataset=train_dataset,
+        #     validation_dataset=val_dataset,
+        #     epochs=150,
+        #     get_best_model=True,
+        #     wandb_project=wandb_project,
+        #     wandb_group='Best-MultiTarget',
+        #     random_seed=seed,
+        #     device=device,
+        # ).run()
+
         if debugger_is_active():
-            train_dataset = MultiTargetDataset(
+            train_dataset = OptimalsDataset(
                 [fp for fp in Path('data/raw/').glob('125_*.json')
                  if (int(fp.name.split('_')[1]) < 20) and
                     (int(fp.name.split('_')[2].replace('.json', '')) < 10)]
             )
             val_dataset = train_dataset
         else:
-            # train_dataset = MultiTargetDataset(
-            #     [fp for fp in Path('data/raw/').glob('125_*.json')
-            #      if (int(fp.name.split('_')[1]) < 20) and
-            #         (int(fp.name.split('_')[2].replace('.json', '')) < 130)]
-            # )
-            # val_dataset = MultiTargetDataset(
-            #     [fp for fp in Path('data/raw/').glob('125_*.json')
-            #      if (int(fp.name.split('_')[1]) < 20) and
-            #         (int(fp.name.split('_')[2].replace('.json', '')) >= 130) and
-            #         (int(fp.name.split('_')[2].replace('.json', '')) < 160)]
-            # )
-            train_dataset = MultiTargetDataset.from_file_lazy('data/processed/multitarget_125_train.hdf5')
-            val_dataset = MultiTargetDataset.from_file_lazy('data/processed/multitarget_125_val.hdf5')
+            train_dataset = OptimalsDataset.from_file_lazy('data/processed/optimals_125_train.hdf5')
+            val_dataset = OptimalsDataset.from_file_lazy('data/processed/optimals_125_val.hdf5')
+
 
         net = OptSatGNN(
-            n_h_feats=256,
+            n_h_feats=8,
             single_conv_for_both_passes=True,
-            n_passes=3,
+            n_passes=1,
             conv1='SAGEConv',
             conv1_kwargs={ 'aggregator_type': 'pool' },
             conv2=None,
             conv2_kwargs=dict(),
         )
 
-        # train_dataset = MultiTargetDataset.from_file_lazy('data/processed/multitarget_125_97_small_all_200.hdf5').get_split('all')
-        # val_dataset = MultiTargetDataset.from_file_lazy('data/processed/multitarget_125_large_all.hdf5').get_split('val')
-        MultiTargetTrainer(
+        OptimalsTrainer(
             net.double(),
             training_dataset=train_dataset,
             validation_dataset=val_dataset,
             epochs=150,
             get_best_model=True,
             wandb_project=wandb_project,
-            wandb_group='Best-MultiTarget',
+            wandb_group='TEST',
             random_seed=seed,
             device=device,
         ).run()
-
-        # # train_dataset = OptimalsDataset(instances_fpaths=instances_fpaths,
-        # #                                 sols_dir='/home/bruno/sat-gnn/data/interim',
-        # #                                 split='train')
-        # train_dataset = OptimalsDataset.from_file_lazy('data/processed/optimals_97_all.hdf5').get_split('train')
-        # # val_dataset = OptimalsDataset(instances_fpaths=instances_fpaths,
-        # #                               sols_dir='/home/bruno/sat-gnn/data/interim',
-        # #                               split='val')
-        # val_dataset = OptimalsDataset.from_file_lazy('data/processed/optimals_97_all.hdf5').get_split('val')
-        # OptimalsTrainer(
-        #     net.double(),
-        #     training_dataset=train_dataset,
-        #     validation_dataset=val_dataset,
-        #     epochs=100,
-        #     wandb_project=wandb_project,
-        #     wandb_group='Optimals + GATv2 + PreNorm',
-        #     random_seed=seed,
-        #     device=device,
-        # ).run()
 
         # train_dataset = VarOptimalityDataset.from_file_lazy('data/processed/varoptimality_97_all.hdf5').get_split('train')
         # train_dataset.samples_per_instance = 20
---
+++ tune_optimals.py
@@ -0,0 +1,85 @@
+import os
+from pathlib import Path
+from time import time
+import pickle
+import numpy as np
+import torch
+import torch.nn
+from tqdm import tqdm
+
+from itertools import product
+from random import shuffle
+
+from src.net import OptSatGNN
+from src.trainer import OptimalsTrainer
+from src.utils import debugger_is_active
+from src.dataset import OptimalsDataset
+
+
+def product_dict(**kwargs):
+    """From https://stackoverflow.com/a/5228294/7964333."""
+    keys = kwargs.keys()
+    for instance in product(*kwargs.values()):
+        yield dict(zip(keys, instance))
+
+if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    wandb_project = 'sat-gnn'
+    wandb_group = 'GridSearch-Optimals'
+
+    memory_size_gb = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / (1024**3)
+    if (memory_size_gb < 30) or debugger_is_active():
+        train_dataset = OptimalsDataset.from_file_lazy('data/processed/optimals_125_train.hdf5')
+        val_dataset = OptimalsDataset.from_file_lazy('data/processed/optimals_125_val.hdf5')
+    else:
+        train_dataset = OptimalsDataset(
+            [fp for fp in Path('data/raw/').glob('125_*.json')
+                if (int(fp.name.split('_')[1]) < 20) and
+                (int(fp.name.split('_')[2].replace('.json', '')) < 200)]
+        )
+        val_dataset = OptimalsDataset(
+            [fp for fp in Path('data/raw/').glob('125_*.json')
+                if (int(fp.name.split('_')[1]) >= 20) and
+                (int(fp.name.split('_')[2].replace('.json', '')) < 20)]
+        )
+        train_dataset.maybe_initialize()
+        val_dataset.maybe_initialize()
+
+    hp_ranges = {
+        'lr': [1e-2, 1e-3, 1e-4],
+        'n_h_feats': [2**5, 2**6, 2**7, 2**8],
+        'single_conv_for_both_passes': [True, False],
+        'n_passes': [1, 2, 3],
+        'conv1': ['GraphConv', 'SAGEConv'],
+        'conv2': ['GraphConv', 'SAGEConv', None],
+        # 'conv3': ['GraphConv', 'SAGEConv'],
+    }
+
+    candidate_hps = product_dict(**hp_ranges)
+    candidate_hps = list(candidate_hps)
+    shuffle(candidate_hps)
+
+    for hps in tqdm(candidate_hps):
+        lr = hps.pop('lr')
+
+        for c in ['conv1', 'conv2']:
+            # try:
+            if hps[c] == 'SAGEConv':
+                hps[c+'_kwargs'] = {'aggregator_type': 'pool'}
+            else:
+                hps[c+'_kwargs'] = dict()
+            # except:
+            #     pass
+
+        for _ in range(1):  # number of runs
+            OptimalsTrainer(
+                OptSatGNN(**hps),
+                train_dataset,
+                val_dataset,
+                lr=lr,
+                epochs=10,
+                get_best_model=True,
+                wandb_project=wandb_project,
+                wandb_group=wandb_group,
+            ).run()
