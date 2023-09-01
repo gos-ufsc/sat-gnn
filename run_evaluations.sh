43f6e28e2e1aa246aefd6a42cc2dabba355a8d0e
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-09-01 09:21:45 -0300

    handy bash script to run all evaluations given a model id

---
+++ run_evaluations.sh
@@ -0,0 +1,10 @@
+python evaluate.py bs
+python evaluate.py ef $1 200
+python evaluate.py ef $1 500
+python evaluate.py ef $1 1000
+python evaluate.py ws $1 500
+python evaluate.py ws $1 1000
+python evaluate.py ws $1 -1
+python evaluate.py tr $1 200
+python evaluate.py tr $1 500
+python evaluate.py tr $1 1000
