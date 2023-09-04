ee494012005d1a335757efce5acf11c6923f3bc9
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-09-04 16:27:57 -0300

    save test sets

--- save_datasets.py
+++ save_datasets.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
                         if (int(fp.name.split('_')[1]) < 20) and
                            (int(fp.name.split('_')[2].replace('.json', '')) < 200)]
 
-   #  ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
-   #                          sols_dir='/home/bruno/sat-gnn/data/interim')
-   #  ds.maybe_initialize()
-   #  ds.save_dataset('data/processed/multitarget_125_train.hdf5')
+    ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
+                            sols_dir='/home/bruno/sat-gnn/data/interim')
+    ds.maybe_initialize()
+    ds.save_dataset('data/processed/multitarget_125_train.hdf5')
 
     ds = OptimalsDataset(instances_fpaths=instances_fpaths,
                          sols_dir='/home/bruno/sat-gnn/data/interim')
@@ -22,12 +22,27 @@ if __name__ == '__main__':
                         if (int(fp.name.split('_')[1]) >= 20) and
                            (int(fp.name.split('_')[2].replace('.json', '')) < 20)]
 
-   #  ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
-   #                          sols_dir='/home/bruno/sat-gnn/data/interim')
-   #  ds.maybe_initialize()
-   #  ds.save_dataset('data/processed/multitarget_125_val.hdf5')
+    ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
+                            sols_dir='/home/bruno/sat-gnn/data/interim')
+    ds.maybe_initialize()
+    ds.save_dataset('data/processed/multitarget_125_val.hdf5')
 
     ds = OptimalsDataset(instances_fpaths=instances_fpaths,
                          sols_dir='/home/bruno/sat-gnn/data/interim')
     ds.maybe_initialize()
     ds.save_dataset('data/processed/optimals_125_val.hdf5')
+
+    instances_fpaths = [fp for fp in Path('data/raw/').glob('125_*.json')
+                        if (int(fp.name.split('_')[1]) >= 20) and
+                           (int(fp.name.split('_')[2].replace('.json', '')) >= 20) and
+                           (int(fp.name.split('_')[2].replace('.json', '')) < 40)]
+
+    ds = MultiTargetDataset(instances_fpaths=instances_fpaths,
+                            sols_dir='/home/bruno/sat-gnn/data/interim')
+    ds.maybe_initialize()
+    ds.save_dataset('data/processed/multitarget_125_test.hdf5')
+
+    ds = OptimalsDataset(instances_fpaths=instances_fpaths,
+                         sols_dir='/home/bruno/sat-gnn/data/interim')
+    ds.maybe_initialize()
+    ds.save_dataset('data/processed/optimals_125_test.hdf5')
