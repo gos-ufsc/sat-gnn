8ae20624828e30842aec3113a2f08aea04cbd630
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2023-09-14 19:06:02 -0300

    evaluate multiple N on the test set

--- run_heuristic_experiments.sh
+++ run_heuristic_experiments.sh
@@ -1,8 +1,9 @@
-python evaluate.py bs
+python evaluate.py bs $2
 for N in 500 750 1000 1250 1500 1750 2000
 do
-    python evaluate.py ef $1 --n $N
-    python evaluate.py ws $1 --n $N
-    python evaluate.py tr $1 --n $N --delta 0.001
-    python evaluate.py tr $1 --n $N --delta 0.01
+    python evaluate.py ef $1 --n $N $2
+    python evaluate.py ws $1 --n $N $2
+    python evaluate.py tr $1 --n $N --delta 1 $2
+    python evaluate.py tr $1 --n $N --delta 2 $2
+    python evaluate.py tr $1 --n $N --delta 5 $2
 done
